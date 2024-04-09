b953a6bf913eef9bc4a8a8d04a199eaefcdbc27b
adamsocrat  <ycs@min2s.com>
2024-04-09 15:12:01 +0200

    base_doc module added
    (cherry picked from commit 963299dd7a028c794670031edbf1ba10da661b9e)

---
+++ addons/base_document/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Base Document",
+    'summary': 'This module adds additional documents for external reports.',
+    'author': 'Ben',
+    'category': 'Base',
+    'version': '17.0.0.1.1',
+    'depends': ['l10n_din5008'],
+    'data': [
+        'views/report_templates.xml',
+        'data/report_layout.xml',
+    ],
+    'assets': {
+        'web.report_assets_common': [
+            'base_document/static/src/**/*',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'AGPL-3',
+}
