5f95c9cdd9688b3393c8884a1aae98474653c4b3
adamsocrat  <ycs@min2s.com>
2024-05-09 17:06:16 +0200

    Add timezone support for billing period in report template
    Timezone support has been added to the date and time fields for the billing period in the report template. Additionally, a comment was added to the data model to clarify that all dates are stored in UTC and formatted on the client side.

--- addons/base_document/models/customization.py
+++ addons/base_document/models/customization.py
@@ -4,5 +4,6 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    # All dates are stored in UTC and formatted on the client side
     billing_period_start = fields.Datetime(string='BillingPeriodStart')
     billing_period_end = fields.Datetime(string='BillingPeriodEnd')
