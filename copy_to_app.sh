fae13c5c4c4955f3064133db0a60665001bc2cc2
adamsocrat  <ycs@min2s.com>
2024-06-15 00:16:08 +0300

    shell script to copy base_doc addon to main repo

---
+++ copy_to_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE_DIR="/Users/mincom/Developer/odoo-17.0/addons/base_document"
+DEST_DIR="/Users/mincom/IdeaProjects/DynamicTariffApp/backend/odoo_addon"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$DEST_DIR"
+
+# Copy contents from the source to the destination directory
+cp -R "$SOURCE_DIR/"* "$DEST_DIR/"
+
+# Optional: Print a message indicating the operation is complete
+echo "Contents of $SOURCE_DIR have been copied to $DEST_DIR"
